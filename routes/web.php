4385d6b72302d2faefc217a1f5d3d1f54ef76c41
yu jie shen <rock.02130529@gmail.com>
2020-11-03 11:18:07 +0800

    設定路由對應的Controller

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/posts',[\App\Http\Controllers\PostsController::class,'index'])->name('posts.index');
+Route::get('/post',[\App\Http\Controllers\PostsController::class,'show'])->name('posts.show');
+Route::get('/about',[\App\Http\Controllers\PostsController::class,'about'])->name('posts.about');
+Route::get('/contact',[\App\Http\Controllers\PostsController::class,'contact'])->name('posts.contact');
 
