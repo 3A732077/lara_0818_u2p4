b9fd9ab67be9be8f6730ca20b810186463362c69
yu-jie shen <rock.02130529@gmail.com>
2020-11-10 21:36:13 +0800

    migration up 設定

--- database/migrations/2020_11_10_132427_create_posts_table.php
+++ database/migrations/2020_11_10_132427_create_posts_table.php
@@ -13,8 +13,14 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
+
+
         Schema::create('posts', function (Blueprint $table) {
+
+
             $table->id();
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
